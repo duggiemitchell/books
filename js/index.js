dadd00448ead6b16acfdf91724701985fb124a1f
E.D.M <Erica@Ericas-iMac.local>
2016-04-01 18:06:54 -0400

    blah

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(window).load(function(){
+    // var $container = $('.portfolioContainer');
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+// Assigning anchor tags in filter section as value for filter key
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    });
+});
